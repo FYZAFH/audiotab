561ecb37650dacce1424fc0acbc77fc4566d9984
Fan Hao <fh@macdeMac-mini.local>
2025-11-24 00:58:26 +0800

    fix(nodes): add Default impl for Print to satisfy clippy

--- src/nodes/print.rs
+++ src/nodes/print.rs
@@ -7,6 +7,12 @@ pub struct Print {
     label: String,
 }
 
+impl Default for Print {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Print {
     pub fn new() -> Self {
         Self {
