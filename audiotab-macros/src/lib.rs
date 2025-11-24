7891f03b01dbb0a81946620035eaa5519507cad0
Fan Hao <fh@macdeMac-mini.local>
2025-11-24 16:46:10 +0800

    feat(macros): create proc_macro crate structure

---
+++ audiotab-macros/src/lib.rs
@@ -0,0 +1,7 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(StreamNode)]
+pub fn derive_stream_node(_input: TokenStream) -> TokenStream {
+    // Will implement in next task
+    TokenStream::new()
+}
